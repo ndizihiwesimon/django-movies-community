bbcc5fd91ce455e2adaed6fefd9b4f1146e8603d
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-08-02 15:26:25 +0200

    Login template

--- member/urls.py
+++ member/urls.py
@@ -3,5 +3,5 @@ from member import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('register', views.Register, name='register')
+    path('register', views.register, name='register')
 ]

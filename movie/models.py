aa4725865102509c422b28ec9ffeb2112ea3863f
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-08-02 14:58:28 +0200

    Models reviewed

--- movie/models.py
+++ movie/models.py
@@ -9,6 +9,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255, blank=False)
     body = models.TextField(blank=False)
     poster = models.ImageField(upload_to="Posters/")
+    genre = models.CharField(max_length=100, blank=False)
+    actors = models.CharField(max_length=100, blank=False)
     trailer = models.URLField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="movies")
     created_at = models.DateTimeField(auto_now_add=True)

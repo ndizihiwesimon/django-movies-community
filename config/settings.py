be8c611d1b6482b0ce2667bc238f73869a230cc3
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-08-02 14:56:25 +0200

    Movie models

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'member',
+    'movie',
 ]
 
 MIDDLEWARE = [
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,17 @@
+from webbrowser import get
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    body = models.TextField(blank=False)
+    poster = models.ImageField(upload_to="Posters/")
+    trailer = models.URLField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="movies")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

578a685db7886b2ddc12e777237b0fdb46560374
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-08-13 18:51:59 +0200

    Modified root

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('member.urls')),
+    path('', include('member.urls')),
     path('movies/', include('movie.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

d30db41418bad1dd2512b18488a2f6ec7a682f6c
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-08-13 18:12:36 +0200

    freezed requirements

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
